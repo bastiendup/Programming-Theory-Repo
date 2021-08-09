51f337fcdc9eff2b601dcd4e63cd4e63d283c917
bastiendup <82089335+bastiendup@users.noreply.github.com>
2021-08-09 22:58:36 +0200

    Knight PM
    added knight possible moves

--- Programming Theory Project/Assets/Scripts/Chessman/Rook.cs
+++ Programming Theory Project/Assets/Scripts/Chessman/Rook.cs
@@ -4,5 +4,113 @@ using UnityEngine;
 
 public class Rook : Chessman
 {
-    
+    public override bool[,] PossibleMove()
+    {
+        bool[,] r = new bool[8, 8];
+
+        Chessman c;
+        int i;
+
+        //Right
+        i = CurrentX;
+        while (true)
+        {
+            i++;
+            if (i >= 8)
+                break;
+
+            c = BoardManager.Instance.Chessmans[i, CurrentY];
+
+            //If there is no piece
+            if (c == null)
+                r[i, CurrentY] = true;
+
+            //If there is a piece
+            else
+            {
+                //If the piece is on the opposit team
+                if (c.isWhite != isWhite)
+                    r[i, CurrentY] = true;
+
+                break;
+            }
+        }
+
+        //Left
+        i = CurrentX;
+        while (true)
+        {
+            i--;
+            if (i < 0)
+                break;
+
+            c = BoardManager.Instance.Chessmans[i, CurrentY];
+
+            //If there is no piece
+            if (c == null)
+                r[i, CurrentY] = true;
+
+            //If there is a piece
+            else
+            {
+                //If the piece is on the opposit team
+                if (c.isWhite != isWhite)
+                    r[i, CurrentY] = true;
+
+                break;
+            }
+        }
+
+        //Up
+        i = CurrentY;
+        while (true)
+        {
+            i++;
+            if (i >= 8)
+                break;
+
+            c = BoardManager.Instance.Chessmans[CurrentX, i];
+
+            //If there is no piece
+            if (c == null)
+                r[CurrentX, i] = true;
+
+            //If there is a piece
+            else
+            {
+                //If the piece is on the opposit team
+                if (c.isWhite != isWhite)
+                    r[CurrentX, i] = true;
+
+                break;
+            }
+        }
+
+        //Down
+        i = CurrentY;
+        while (true)
+        {
+            i--;
+            if (i < 0)
+                break;
+
+            c = BoardManager.Instance.Chessmans[CurrentX, i];
+
+            //If there is no piece
+            if (c == null)
+                r[CurrentX, i] = true;
+
+            //If there is a piece
+            else
+            {
+                //If the piece is on the opposit team
+                if (c.isWhite != isWhite)
+                    r[CurrentX, i] = true;
+
+                break;
+            }
+        }
+
+        return r;
+    }
 }
